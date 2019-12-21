c2322328dfe218b2c34c87d6e8d919f9570b5f8c
Nathan Brickett <natey37@gmail.com>
2019-12-21 04:47:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -252,7 +252,7 @@ def player_with_longest_name
 end 
 
 def long_name_steals_a_ton?
-  longest_name = player_with_the_longest_name
+  longest_name = player_with_longest_name
   steals = 0 
   game_hash.each do |home_or_away, team|
     team.each do |attribute, data|
